250e57d3bbe3900b844ffcb59c96a3ffad4673d2
tianwen <yaohii@foxmail.com>
2024-12-19 22:19:06 +0800

    Update main.rs

--- RustLang/test/src/main.rs
+++ RustLang/test/src/main.rs
@@ -2,11 +2,19 @@ use std::collections::HashMap;
 use std::fmt::Display;
 
 fn main() {
-    let a = A{x:1,y:2};
+    /*let a = A{x:1,y:2};
     a.to_string_my();
     a.show();
     let b = re(&1,&2);
-    println!("Hello, world!{}",b);
+    println!("Hello, world!{}",b);*/
+    
+    let sub_class = SubClass{
+        base:BaseClass{
+            id:1,
+        }
+    };
+    println!("{}",sub_class.get_id());
+    
 }
 //生命周期
 //1. 由于有多个引用的时候，编译器不知道哪个会返回，导致编译器对生命周期很懵
@@ -40,5 +48,30 @@ impl<T> MyTrait for T
 {
     fn to_string_my(&self) {
         println!("第二个实现");
+    }
+}
+
+
+//trait 关联类型
+struct BaseClass{
+    id:i32
+}
+struct SubClass{
+    base: BaseClass
+}
+trait Id{
+    fn get_id()->i32;
+}
+
+trait BaseTrait{
+    type Base;
+    fn get_id(&self)->i32;
+}
+
+impl BaseTrait for SubClass {
+    type Base = BaseClass;
+
+    fn get_id(&self) -> i32 {
+        return self.base.id;
     }
 }
